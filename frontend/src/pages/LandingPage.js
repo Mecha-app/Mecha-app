93a9d6e2cb5056aaf2ec1f13a5e52b5762da9faf
frantzmathieu03 <frantz.mathieu@yahoo.com>
2026-05-29 16:01:23 -0400

    Fix getLang import in LandingPage

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import{useEffect,useState,useRef}from'react';
 import{useNavigate}from'react-router-dom';
 import{useAuth}from'../context/AuthContext';
+import{LANGUAGES,getLang,setLang}from'../utils/languages';
 
 export default function LandingPage(){
   const{user}=useAuth();
